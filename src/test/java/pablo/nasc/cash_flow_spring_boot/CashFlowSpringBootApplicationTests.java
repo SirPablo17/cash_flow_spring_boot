e123a4f66f425a901b1902270d4b6a77fda767d0
PABLO NASCIMENTO DO ROSARIO <pablo.nrosario@senacsp.edu.br>
2026-03-31 20:09:15 -0300

    Initial commit

---
+++ src/test/java/pablo/nasc/cash_flow_spring_boot/CashFlowSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pablo.nasc.cash_flow_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
