d4a35970f35721080a2bf7193f439b5bf0a82ef1
PABLO NASCIMENTO DO ROSARIO <pablo.nrosario@senacsp.edu.br>
2026-03-31 20:09:15 -0300

    Initial commit

---
+++ src/main/java/pablo/nasc/cash_flow_spring_boot/CashFlowSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pablo.nasc.cash_flow_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashFlowSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashFlowSpringBootApplication.class, args);
+	}
+
+}
